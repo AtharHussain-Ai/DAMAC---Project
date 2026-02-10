b72f7ab29d8e124c3f2cf3b9e8cb971bf1083964
Mohammad Athar Hussain <athar.ai.dev@gmail.com>
2026-02-10 12:33:44 +0530

    added section1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const btn = document.getElementById("menuBtn");
 const overlay = document.getElementById("menuOverlay");
 
@@ -39,3 +41,61 @@ btn.addEventListener("click",()=>{
   }
 
 });
+
+
+function page1(){
+  window.addEventListener("load", () => {
+
+  gsap.registerPlugin(SplitText);
+
+  const tl = gsap.timeline({
+    defaults:{ ease:"power4.out" }
+  });
+
+  /* ===== TITLE ===== */
+  tl.to(".about_us_sec",{
+    opacity:1,
+    y:0,
+    duration:1
+  });
+
+  /* ===== SPLIT HERO TEXT ===== */
+  const split = new SplitText(".para1", {
+    type:"lines",
+    linesClass:"split-line"
+  });
+
+  split.lines.forEach(line=>{
+    const mask = document.createElement("div");
+    mask.classList.add("line-mask");
+    line.parentNode.insertBefore(mask, line);
+    mask.appendChild(line);
+  });
+
+  tl.to(".para1",{ opacity:1, duration:0.2 });
+
+  tl.from(split.lines,{
+    yPercent:120,
+    duration:1.25,
+    stagger:0.12
+  },"-=0.4");
+
+  /* ===== MUTED WORDS ===== */
+  tl.from(".muted",{
+    opacity:0,
+    duration:1.1,
+    stagger:0.08
+  },"-=0.8");
+
+  /* ===== BOTTOM TEXT ===== */
+  tl.to(".bottom p",{
+    opacity:1,
+    y:0,
+    duration:1.2,
+    stagger:0.25
+  },"-=0.5");
+
+});
+}
+
+page1()
